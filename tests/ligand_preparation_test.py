09e5404bdbb4f3503275fa3d95bc0ba828a96c6e
lwantoch <l.wantoch@icloud.com>
2026-02-09 12:04:52 +0100

    Addind test with data for ligand preparation module

--- tests/ligand_preparation_test.py
+++ tests/ligand_preparation_test.py
@@ -1,11 +1,34 @@
 from __future__ import annotations
-from src.gbsa_pipeline.ligand_preparation import load_ligand_sdf, ligand_standardizer
+import pytest
+import BioSimSpace as BSS
+from src.gbsa_pipeline.ligand_preparation import (
+    load_ligand_sdf,
+    ligand_standardizer,
+    ligand_converter,
+)
+
+
+def test_load_ligand_valid() -> None:
+    """Test reading molecule from file"""
+    mol = load_ligand_sdf("testdata/complex3.sdf")
+    assert mol != None
+
+
+def test_load_ligand_empty_raises() -> None:
+    with pytest.raises(ValueError):
+        load_ligand_sdf("testdata/empty.sdf")
 
 
 def test_hydrogens_added() -> None:
     """Test standardization adds correct number of hydrogens"""
-    mol = load_ligand_sdf("data/complex3/complex3.sdf")
+    mol = load_ligand_sdf("testdata/complex3.sdf")
     mol = ligand_standardizer(mol)
 
     n_atoms = mol.GetNumAtoms()
     assert n_atoms == 72
+
+
+def test_ligand_conversion() -> None:
+    """Test conversion of the ligand"""
+    mol = ligand_converter("testdata/complex3.sdf")
+    assert isinstance(mol, BSS._SireWrappers.Molecule)
