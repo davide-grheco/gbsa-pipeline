02ce92af8723d6765d43ff80bde86d140d0c011e
lwantoch <l.wantoch@icloud.com>
2026-02-09 15:41:50 +0100

    correcting testdata directory

--- tests/ligand_preparation_test.py
+++ tests/ligand_preparation_test.py
@@ -11,13 +11,13 @@ from src.gbsa_pipeline.ligand_preparation import (
 def test_load_ligand_valid() -> None:
     """Test reading molecule from file"""
 
-    mol = load_ligand_sdf("testdata/complex3.sdf")
+    mol = load_ligand_sdf("tests/testdata/complex3.sdf")
     assert mol is not None
 
 
 def test_hydrogens_added() -> None:
     """Test standardization adds correct number of hydrogens"""
-    mol = load_ligand_sdf("testdata/complex3.sdf")
+    mol = load_ligand_sdf("tests/testdata/complex3.sdf")
     mol = ligand_standardizer(mol)
 
     n_atoms = mol.GetNumAtoms()
@@ -27,10 +27,10 @@ def test_hydrogens_added() -> None:
 def test_read_empty() -> None:
 
     with pytest.raises(OSError):
-        load_ligand_sdf("testdata/empty.sdf.sdf")
+        load_ligand_sdf("tests/testdata/empty.sdf.sdf")
 
 
 def test_ligand_conversion() -> None:
     """Test conversion of the ligand"""
-    mol = ligand_converter("testdata/complex3.sdf")
+    mol = ligand_converter("tests/testdata/complex3.sdf")
     assert isinstance(mol, BSS._SireWrappers.Molecule)
