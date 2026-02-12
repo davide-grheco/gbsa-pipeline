0a6ddf0f286d595d5864430cdb2469e8499185c0
Davide Crucitti <141631141+davide-grheco@users.noreply.github.com>
2026-02-12 15:16:04 +0000

    Switch from conda to pixi (#6)
    * Switched from conda to pixi
    
    Better package manager that allows using pyproject configuration as well as pypi packages.
    
    * Keep only the dev environment
    
    * Remove unused github templates
    
    * Delete template tests
    
    * Delete template code
    
    * Remove credits page from documentation
    
    ---------
    
    Co-authored-by: lwantoch <l.wantoch@icloud.com>

--- src/gbsa_pipeline/__init__.py
+++ src/gbsa_pipeline/__init__.py
@@ -4,7 +4,3 @@ Optimize GBSA calculations for speed
 """
 
 from __future__ import annotations
-
-from gbsa_pipeline._internal.cli import get_parser, main
-
-__all__: list[str] = ["get_parser", "main"]
