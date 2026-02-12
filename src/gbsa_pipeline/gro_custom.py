8d9b2e4a79d907c1f139e018d3d3300c8cda5b1d
lwantoch <l.wantoch@icloud.com>
2026-02-12 17:16:58 +0100

    adding custom run functionality
    Better package manager that allows using pyproject configuration as well as pypi packages.

--- src/gbsa_pipeline/gro_custom.py
+++ src/gbsa_pipeline/gro_custom.py
@@ -1,10 +1,28 @@
 """Preparing custom GROMACS protocol for GBSA optimization."""
 
+import logging
+from pathlib import Path
+
 import BioSimSpace as BSS
 
 
-class GROMACS_custom(BSS.Protocol.Custom):
+class GromacsCustom(BSS.Protocol.Custom):
     """Thin wrapper for a custom GROMACS protocol."""
 
-    def __init__(self, config):
+    def __init__(self, config: str | Path):
+        """Constructin an object."""
         super().__init__(config)
+
+
+def run_gro_custom(parameters: str | Path, system: BSS._SireWrappers.System) -> BSS._SireWrappers.System:
+    """Creates custom GROMACS protocol and runs simulation."""
+    custom_protocol = GromacsCustom(parameters)
+    logging.info("Created Protocol")
+    proces = BSS.Process.Gromacs(system, protocol=custom_protocol)
+    logging.info("Starting proces")
+    proces.start()
+    proces.wait()
+    logging.info("Proces finished")
+    customized = proces.getSystem(block=True)
+
+    return customized
